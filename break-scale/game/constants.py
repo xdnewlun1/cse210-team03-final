c30e7e8904bd96bb78e56ca673fdbe492e48e1ed
awarbler <awoodfor@byui.edu>
2021-11-15 09:43:15 -0600

    updating constants

--- break-scale/game/constants.py
+++ break-scale/game/constants.py
@@ -1,2 +1,19 @@
+import arcade
 SCREEN_WIDTH = 800
 SCREEN_HEIGH = 600
+SCREEN_TITLE = "Break the Scale"
+
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+FOOD_SCALING = 0.5
+
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 5
+
+
+# Player starting position
+
+# Layer Names from our TileMap
+
+
