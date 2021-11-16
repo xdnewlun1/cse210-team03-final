fb396e92c1fd2f500bd0fc53587a897a830c64de
awarbler <awoodfor@byui.edu>
2021-11-15 21:42:00 -0600

    add player on key press release an notes

--- break-scale/game/player.py
+++ break-scale/game/player.py
@@ -5,6 +5,14 @@ class Player:
 
     def __init_ (self):
         # Set up the player, specifically placing it at these coordinates.
-
+ 
         pass
 
+
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+        pass
+     def on_key_release(self, key, modifiers):
+        """Called when the user releases a key."""
+        pass
