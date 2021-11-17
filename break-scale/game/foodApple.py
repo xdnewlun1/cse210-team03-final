517a9571725bf12def39d6f003acf1b58eeee576
awarbler <awoodfor@byui.edu>
2021-11-17 13:21:08 -0600

    added notes to foodApple

---
+++ break-scale/game/foodApple.py
@@ -0,0 +1,12 @@
+class Food: # this needs to have parameter of arcade library and sprite 
+    """This class represents the food on our screen. it is a chld class
+    of the arcade librarys Sprite class"""
+
+    #def reset_pos to reset the position calling self
+    """reset the food to a random spot above the screen"""
+
+    # define update to move the food 
+    """move the food and see if the food has fallen off the 
+        bottom of the screen. if so reset it"""
+    pass
+
