5b14e037466f6ff44748688b69d9f4177e7dd852
awarbler <awoodfor@byui.edu>
2021-11-16 19:04:07 -0600

    updated director and constants

--- break-scale/game/constants.py
+++ break-scale/game/constants.py
@@ -4,10 +4,14 @@ import os
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Break the Scale"
+
 #constants used to scale our sprites from their original size 
 CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 FOOD_SCALING = 0.5
+
+# do we need gravity? 
+
 # MOVEMENT SPEED OF PLAYER 
 PLAYER_MOVEMENT_SPEED = 10 
 
@@ -21,8 +25,10 @@ PLAYER_MOVEMENT_SPEED = 5
 
 # FOOD COUNT 
 FOOD_COUNT = 50
+
 # PLAYER STARTING POSITION
 
 # LAYER NAMES FROM TILEMAP
 
+# file path
 PATH = os.path.dirname(os.path.abspath(__file__))
