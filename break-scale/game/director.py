e815ed031149fd0567611b5d0b2250330b40580a
awarbler <awoodfor@byui.edu>
2021-11-17 13:23:09 -0600

    director code

--- break-scale/game/director.py
+++ break-scale/game/director.py
@@ -15,21 +15,23 @@ class Director(arcade.Window):
         super().__init__(constants.SCREEN_WIDTH, constants.SCREEN_WIDTH, constants.SCREEN_TITLE)
         
         # our scene object to initialize and draw all at once 
-
+        self.scene = None
         # notes about scene object the sprite list would be different
-        # we would set scene.add_sprite_list("Player")
-
+        # we would set scene.add_sprite_list("Player") add it to setup
+        
         # create the variable that will hold the sprite list 
         # These are list that keep track of sprites
         # Each sprite should go into a list set to none 
         # player_sprite_list, apple_sprite_list, donut_sprite_list, spaital hashing off
         # wall_list_sprite turn on spatial hashing
-        self.player_list = None
-        self.apple_sprite_list = None
-        self.snowflake_sprite_list = None # snowflake example
+        # self.player_list = None
+        # self.apple_sprite_list = None
+        # self.snowflake_sprite_list = None # snowflake example
 
         # separate variable that holds the player sprite
         self.player_sprite = None
+
+        # our physics engine
 
         # set background color or set the tileMap
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
@@ -37,22 +39,30 @@ class Director(arcade.Window):
     def setup(self):
         """set up the game here. call this function to restart the game """
         # initialize in init_ add in set up for different levels
-
+        # initialize the set up for scene objects
+        # we would set scene.add_sprite_list("Player")
+        self.scene = arcade.Scene()
         # setup the cameras
 
-        # sprite list create the object instance 
-        self.player_list = arcade.SpriteList()
-        self.apple_sprite_list = arcade.SpriteList()
+        # create the sprite list for scene object scene
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Apple")
+        self.scene.add_sprite_list("Walls")
+        self.scene.add_sprite_list("Snowflake")
+
+        # sprite list create the object instance for individual 
+        # self.player_list = arcade.SpriteList()
+        # self.apple_sprite_list = arcade.SpriteList()
 
         # set up the player instance, 
         # character scaling to .50 for 50 % of the screen
         # set up player specifically placing it at the coordinates center x 
         # and center y coordinates 
-        image_source = ":resources:images/player/alienGreen_stand.png"
+        image_source = "break-scale/game/resources/images/player/alienGreen_stand.png"
         self.player_sprite = arcade.Sprite(image_source, constants.CHARACTER_SCALING)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
-        self.player_list.append(self.player_sprite)
+        self.player_sprite.center_x = 400
+        self.player_sprite.center_y =150
+        self.scene.add_sprite("Player", self.player_sprite)
     
         # keep track of score
         #self.score = 0 set to zero here and in initialize 
@@ -99,6 +109,7 @@ class Director(arcade.Window):
 
             # add snowflake to snowflake list 
             self.snowflake_list.append(snowflake)
+            
 
         #don't show the mouse pointer # snowflake ex
         self.set_mouse_visible(False)
@@ -109,35 +120,57 @@ class Director(arcade.Window):
         # create the physics engine by setting it to arcades physcis engine
         # and adding the player sprite and the scene sprite list to walls
 
-        
-    
     def on_draw(self):
         """render the screen"""
         # clear the screen to the background color
         arcade.start_render()
 
         # draw our sprites or call draw the scene draw ()
-        self.player_list.draw()
+        self.scene.draw()
         #self.apple_list.draw()
+
+        # put instructions on screen 
+        # instructions = "Move left or right arrows to get eat food"
+        # arcade.draw_text(instructions, 10,90, arcade.color.WHITE,12)
+        
 
         # activate the game camera
 
         # draw our score on the screen, scrolling it with the viewport
-
+        # player explodes
         # snowflake example
         # draw the current position of each snowflake
         for snowflake in self.snowflake_list:
             arcade.draw_circle_filled(snowflake.x, snowflake.y, snowflake.size, arcade.color.WHITE)
 
+    def on_key_press(self, key, modifiers):
+        """ called whenever a key on the keyboard is pressed
+        for a list of keys, see:https://api.arcade.academy/en/latest/arcade.key.html """
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = - constants.PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x == constants.PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """ called whenever the user lets off a previously pressed key basically stops the player from moving"""
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x == 0
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """called whenever the mouse moves"""   
+        pass
+    def on_mouse_press(self, x,y,delta_x, delta_y):
+        """called whenthe user pressesthe mouse button"""
+        pass
+    def on_mouse_release(self, x,y,delta_x, delta_y):
+        """called when a u ser releases a mouse button"""
+        pass
 
     def on_update(self, delta_time):
         """movement and game logic"""
-        # move the player with the physics engine
-        # see if we hit any food
-        #loop through each food we hit if any and remove it 
-        #position the camera
-        
-        # snowflake example 
+         # snowflake example 
         # Animate all the snowflake falling
         for snowflake in self.snowflake_list:
             snowflake.y -= snowflake.speed * delta_time
@@ -149,22 +182,29 @@ class Director(arcade.Window):
             # some math to make the snowflake move side to side
             snowflake.x += snowflake.speed * math.cos(snowflake.angle) * delta_time
             snowflake.angle += 1 * delta_time
+            
+            # where do we put the key presses ??? in player?  input? or leave here? 
+        # move the player with the physics engine
 
 
-    # where do we put the key presses ??? in player?  input? or leave here? 
-    def on_key_press(self, key, key_modifiers):
-        """ called whenever a key on the keyboard is pressed
-        for a list of keys, see:https://api.arcade.academy/en/latest/arcade.key.html """
-        pass
-    def on_key_release(self, key, key_modifiers):
-        """ called whenever the user lets off a previously pressed key"""
-        pass
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """called whenever the mouse moves"""   
-        pass
-    def on_mouse_press(self, x,y,delta_x, delta_y):
-        """called whenthe user pressesthe mouse button"""
-        pass
-    def on_mouse_release(self, x,y,delta_x, delta_y):
-        """called when a u ser releases a mouse button"""
-        pass
+        # move player without physics list 
+        # move player
+        # remove these lines in physics enige is moving player
+        self.player_sprite.center_x += self.player_sprite.change_x
+        self.player_sprite.center_y += self.player_sprite.change_y
+
+        #check for out of bounds
+        if self.player_sprite.left < 0:
+            self.player_sprite.left = 0
+        elif self.player_sprite.right > constants.SCREEN_WIDTH -1:
+            self.player_sprite.right = constants.SCREEN_WIDTH -1
+
+        if self.player_sprite.bottom < 0:
+            self.player_sprite.bottom = 0
+        elif self.player_sprite.top > constants.SCREEN_HEIGHT -1 :
+            self.player_sprite.top = 0
+        # see if we hit any food
+        #loop through each food we hit if any and remove it 
+        #position the camera
+
+        # Generate a list of all sprites that collided with the player.
