f927408f00d2ecd6ba8cdbd6fe89a96ef6095e93
awarbler <awoodfor@byui.edu>
2021-11-24 21:07:20 -0600

    added falling apples woohoo

--- break-scale/game/foodApple.py
+++ break-scale/game/foodApple.py
@@ -1,44 +1,27 @@
 import arcade
 from game import constants
 import random
+
+
 class Apple(arcade.Sprite): 
     """This class represents the food on our screen. it is a child class
     of the arcade library Sprite class"""
 
-    #def __init__(self, arcade, sprite):
-        # self.arcade = arcade
-        # self.sprite = sprite
-        # self.x = 0
-        # self.y = 0
-    #def reset_pos to reset the position calling self
-    """reset the food to a random spot above the screen"""
-
-    def reset_pos(self):
-        # self.y = constants.SCREEN_HEIGHT + 50
-        # self.x = random.randrange(constants.SCREEN_WIDTH)
-        self.center_y = random.randrange(constants.SCREEN_HEIGHT + 30, constants.SCREEN_HEIGHT + 100)
-        self.center_x = random.randrange(constants.SCREEN_WIDTH)
-
-
-        
-    def update(self, delta_time):
+    def update(self):
             # define update to move the food 
         """move the food and see if the food has fallen off the 
         bottom of the screen. if so reset it"""
         
-        # self.y -= constants.FOOD_FALL_SPEED * delta_time
-        # if(self.y < 0):
-        #     self.reset_pos()
         self.center_y -= 1
 
         
         # did the food go off the screen If so, pop back to the top.
         # or remove it
         if self.top < 0:
-            self.reset_pos()
+            # self.reset_pos()
+            self.bottom = constants.SCREEN_HEIGHT
 
     """
         Copy Falling Snow to be falling apples that collide with the player and give points.
         Colision
-        https://upload.wikimedia.org/wikipedia/commons/3/3e/Applepix.png
-    """
+        https://upload.wikimedia.org/wikipedia/commons/3/3e/Applepix.png"""
