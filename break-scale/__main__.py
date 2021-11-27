f38707e1e816a751dc0ab7c682f6e98d50020a35
zrobker <zrobker@byui.edu>
2021-11-27 13:11:37 -0500

    Game ends and starting screen

--- break-scale/__main__.py
+++ break-scale/__main__.py
@@ -1,10 +1,12 @@
 import arcade
-from game.director import Director
+from game import constants 
+from game.instructionsView import InstructionsView
 
 def main():
     """ call dirctor , setup() and arcade run()"""
-    window = Director()
-    window.setup()
+    window = arcade.Window(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
+    start_view = InstructionsView()
+    window.show_view(start_view)
     arcade.run()
 
 
--- break-scale/game/instructionsView.py
+++ break-scale/game/instructionsView.py
@@ -1,4 +1,26 @@
 import arcade
-class Instructions(arcade.View):
+from game.director import GameView
+class InstructionsView(arcade.View):
     """This is to show different views for a windows
     """
+    def on_show(self):
+        """ This is run once when we switch to this view """
+        arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
+
+        # Reset the viewport, necessary if we have a scrolling game and we need
+        # to reset the viewport back to the start so we can see what we draw.
+        arcade.set_viewport(0, self.window.width, 0, self.window.height)
+
+    def on_draw(self):
+        """ Draw this view """
+        arcade.start_render()
+        arcade.draw_text("Instructions Screen", self.window.width / 2, self.window.height / 2,
+                         arcade.color.WHITE, font_size=50, anchor_x="center")
+        arcade.draw_text("Click to advance", self.window.width / 2, self.window.height / 2-75,
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """ If the user presses the mouse button, start the game. """
+        game_view = GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
