6dd55c8d6ee0fdf4548906ddd1e4df1c0be26d8c
zrobker <zrobker@byui.edu>
2021-11-30 11:35:53 -0500

    Main Menu and Instructions

--- break-scale/__main__.py
+++ break-scale/__main__.py
@@ -1,11 +1,11 @@
 import arcade
 from game import constants 
-from game.instructionsView import InstructionsView
+from game.menuView import MenuView
 
 def main():
     """ call dirctor , setup() and arcade run()"""
     window = arcade.Window(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
-    start_view = InstructionsView()
+    start_view = MenuView()
     window.show_view(start_view)
     arcade.run()
 
