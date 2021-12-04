381270aad96efaf15ebab33392a4a5cb738d36c1
awarbler <awoodfor@byui.edu>
2021-12-04 16:28:22 -0600

    added barbell code

---
+++ break-scale/game/barbell.py
@@ -0,0 +1,25 @@
+import arcade
+from game import constants
+import random
+
+class Barbell(arcade.Sprite): 
+    """This class represents the food on our screen. it is a child class
+    of the arcade library Sprite class"""
+    def __init__(self) : 
+        super().__init__(constants.BARBELL_SPRITE, constants.FOOD_SCALING)
+        self.center_x = random.randrange(constants.SCREEN_WIDTH)
+        self.center_y = random.randrange(constants.SCREEN_HEIGHT)
+
+    def reset_pos(self):
+        """Rest the fruit once it hits the bottom of the screen"""
+        self.center_y = random.randrange(constants.SCREEN_HEIGHT + 30, constants.SCREEN_HEIGHT + 100)
+        self.center_x = random.randrange(constants.SCREEN_WIDTH)
+
+    def update(self):
+        """move the food and see if the food has fallen off the 
+        bottom of the screen. if so reset it"""
+        self.center_y -= 1
+
+        # did the food go off the screen If so, pop back to the top.
+        if self.top < 0:
+            self.reset_pos()
